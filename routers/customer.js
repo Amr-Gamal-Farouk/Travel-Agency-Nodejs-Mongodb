d82e70f9843f099e3c84a3de3bedf4656675f8a7
Amr-Gamal-Farouk <68358428+Amr-Gamal-Farouk@users.noreply.github.com>
2020-12-06 22:35:53 +0200

    Update customer.js

--- routers/customer.js
+++ routers/customer.js
@@ -5,13 +5,13 @@ const nodemailer = require("nodemailer");
 const bcrypt = require('bcrypt');
 const customer = Router();
 const saltRounds = 10;
-const secretKey = "ezzat";
+const secretKey = "key";
 const transporter = nodemailer.createTransport({
 
     service: 'gmail',
     auth: {
-        user: 'ezzat.ashraf31@gmail.com', // generated ethereal user
-        pass: 'zezoashrf13', // generated ethereal password
+        user: 'email', // generated ethereal user
+        pass: 'password', // generated ethereal password
     },
 });
 
@@ -37,7 +37,7 @@ customer.post('/signup', async (req, res) => {
                 res.status(400).json(error.message);
             } else {
                 let info = transporter.sendMail({
-                    from: ' Company Name <ezzat.ashraf31@gmail.com>', // sender address
+                    from: ' Company Name <email>', // sender address
                     to: body.email,
                     subject: "verification code ",
                     text: "Your verification code is : " + vCode,
@@ -131,7 +131,7 @@ customer.post('/resendmail', async (req, res) => {
         if (userData != null) {
             console.log("dddddd")
             let info = await transporter.sendMail({
-                from: ' Company Name <ezzat.ashraf31@gmail.com>', // sender address
+                from: ' Company Name <email>', // sender address
                 to: email, // list of receivers
                 subject: "verification code" + userData.VCode, // Subject line
                 text: "Your verification code is : ", // plain text body
@@ -160,78 +160,3 @@ customer.post('/resendmail', async (req, res) => {
 })
 
 module.exports = customer;
-
-
-
-
-
-/*
-customer.patch('/addcard/:id', async (req, res) => {
-    const userID=req.url.split('/')[2];
-    console.log(userID);
-    const { body } = req;
-    let userEmail = "";
-    try{
-    jwt.verify(body.token, secretKey, function (err, decoded) {
-        if (!err) {
-            userEmail = decoded.username;
-        } else {
-            throw "access denied"
-        }
-    });
-
-    let userData = await Customer.findOne({ email: userEmail });
-    if (userData != null) {
-        userData.cart.push({tripId:body.tripid,travelerNum:body.travelernum,coast:body.tripcost});
-        console.log({tripId:body.tripid,travelerNum:body.travelernum,coast:body.tripcost},"cart");
-        let userData = await Customer.findOneAndUpdate({ email: userEmail }, userData);
-        if (userData != null) {
-            res.status(200).json({ msg: "UpDate Done" });
-        } else {
-            res.status(400);
-            res.send({ msg: "UpDate Not Done" })
-        }
-    }
-}catch(error){
-    res.status(400);
-    res.send({errormsg:error})
-}
-});
-customer.patch('/deletcard', async (req, res) => {
-    const { body } = req;
-    let userEmail = "";
-    jwt.verify(body.token, secretKey, function (err, decoded) {
-        if (!err) {
-            userEmail = decoded.username;
-        } else {
-            console.log(err)
-            res.status(400);
-            res.send({ msg: "No Access Alowed" })
-        }
-    });
-
-    let userData = await Customer.findOne({ email: userEmail });
-    if (userData != null) {
-        for(let i=0;i<userData.cart.length;i++){
-            console.log( userData.cart[i].tripId,"=", body.tripid )
-            if(userData.cart[i].tripId===body.tripid){
-                console.log(userData.cart[i],"deleted");
-                userData.cart.remove(userData.cart[i]);
-                break;
-            }
-            console.log(userData.cart[i]);
-        }
-
-
-        let data = await Customer.findOneAndUpdate({ email: userEmail }, userData);
-        if (data != null) {
-            res.status(200).json({ msg: "UpDate Done" });
-        } else {
-            res.status(400);
-            res.send({ msg: "UpDate Not Done" })
-        }
-    } else {
-        res.status(400);
-        res.send({ msg: "Wrong Token" })
-    }
-})*/
